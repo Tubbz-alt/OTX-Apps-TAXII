3304f359fe5fbf8c7f608286225c53cb9e708689
Chris Taylor <christ@malfusion.org.uk>
2016-07-13 15:11:05 +0100

    Modified script to use CERT-UK ingest rather than TAXII

---
+++ certukingest.py
@@ -0,0 +1,20 @@
+import json
+
+import requests
+
+
+def _construct_headers():
+    headers = {
+        'Content-Type': 'application/xml',
+        'Accept': 'application/json'
+    }
+    return headers
+
+
+def inbox_package(endpoint_url, stix_package):
+    """Inbox the package to the adapter."""
+    data = stix_package
+    headers = _construct_headers()
+    response = requests.post(endpoint_url, data=data, headers=headers)
+    print(json.dumps(response.json(), indent=4))
+    return
